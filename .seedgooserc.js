ab7cefe49be0df8f80a11a8e2d912dea26a57d1a
carliewhite <carlie.white@snhu.edu>
2022-10-02 13:49:32 -0400

    Module Five

--- .seedgooserc.js
+++ .seedgooserc.js
@@ -1,6 +1,6 @@
 module.exports = {
-    modelBaseDirectory: 'app_server/models',
-    models: ['*.js', '!db.js'],
-    data: 'data',
-    db: 'mongodb://localhost:27017/travlr'
-};
+    modelBaseDirectory: 'app_api/database/models', // model directory name
+    models: ["*.js", "!db.js"], // model matcher
+    data: 'data', // data directory name
+    db: 'mongodb://localhost:27017/travlr' // db connection url
+  };
