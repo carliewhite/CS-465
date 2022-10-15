dd9c33b2d7d132d36e30ed331b288042f91c70ca
carliewhite <carlie.white@snhu.edu>
2022-10-15 17:54:42 -0400

    module 7

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -6,10 +8,13 @@ const logger = require('morgan');
 const hbs = require('hbs');
 require('./app_api/database/db');
 
+require('./app_api/config/passport');
+
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
 const travelRouter = require('./app_server/routes/travel');
 const apiRouter = require('./app_api/routes/index');
+const passport = require('passport');
 
 const app = express();
 
@@ -26,6 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 // Allow CORS
 app.use("/api", (req, res, next) => {
--- app_admin/src/app/services/trip-data.service.ts
+++ app_admin/src/app/services/trip-data.service.ts
@@ -1,10 +1,17 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable, Inject } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
-import { Trip } from '../models/trip';
+import { Trip } from "../models/trip";
+import { BROWSER_STORAGE } from "../storage";
+import { User } from "../models/user";
+import { AuthResponse } from "../models/authresponse";
+
 @Injectable()
 export class TripDataService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    @Inject(BROWSER_STORAGE) private storage: Storage
+  ) {}
 
   private apiBaseUrl = "http://localhost:3000/api/";
   private tripUrl = `${this.apiBaseUrl}trips/`;
@@ -18,36 +25,60 @@ export class TripDataService {
       .catch(this.handleError);
   }
 
-  public getTrip(tripCode: string): Promise<Trip[]> {
-    console.log("Inside TripDataService#getTrip(tripCode)");
+  public getTrip(tripCode: string): Promise<Trip> {
+    console.log("Inside TripDataService#getdTrip");
     return this.http
       .get(this.tripUrl + tripCode)
       .toPromise()
-      .then((response) => response as Trip[])
+      .then((response) => response as Trip)
       .catch(this.handleError);
   }
 
   public addTrip(formData: Trip): Promise<Trip> {
-    console.log("Insided TripDataService#addTrip");
+    console.log("Inside TripDataService#addTrip");
+    const headers = new Headers({
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("travlr-token")}`,
+    });
     return this.http
-      .post(this.tripUrl, formData)
+      .post(this.tripUrl, formData, { HttpHeaders: headers })
       .toPromise()
       .then((response) => response as Trip[])
       .catch(this.handleError);
   }
 
-  public updateTrip(formData: Trip): Promise<Trip[]> {
+  public updateTrip(formData: Trip): Promise<Trip> {
     console.log("Inside TripDataService#updateTrip");
-    console.log(formData);
+    const headers = new Headers({
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("travlr-token")}`,
+    });
     return this.http
-      .put(this.tripUrl + formData.code, formData)
+      .put(this.tripUrl + formData.code, formData, { HttpHeaders: headers })
       .toPromise()
       .then((response) => response as Trip[])
       .catch(this.handleError);
   }
 
   private handleError(error: any): Promise<any> {
-    console.error("Something has gone wrong", error);
+    console.error("Something has gone wrong", error); // for demo purposes only
     return Promise.reject(error.message || error);
+  }
+
+  public login(user: User): Promise<AuthResponse> {
+    return this.makeAuthApiCall("login", user);
+  }
+
+  public register(user: User): Promise<AuthResponse> {
+    return this.makeAuthApiCall("register", user);
+  }
+
+  private makeAuthApiCall(urlPath: string, user: User): Promise<AuthResponse> {
+    const url: string = `${this.apiBaseUrl}/${urlPath}`;
+    return this.http
+      .post(url, user)
+      .toPromise()
+      .then((response) => response as AuthResponse)
+      .catch(this.handleError);
   }
 }
